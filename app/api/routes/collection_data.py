dedd5bf98590a803f897eadbdb0c8048eba94a16
Nikola Nikolovski <nikolovski.nikola42@gmail.com>
2025-03-11 21:22:33 +0100

    Enhance MongoDB Data Handling with Pagination
    - Implemented pagination for MongoDB collections.
    - Introduced CollectionRegister, a dictionary mapping collection names to specific classes.
    - Updated collection_data route to support fetching paginated data from any collection.

--- app/api/routes/collection_data.py
+++ app/api/routes/collection_data.py
@@ -1,38 +1,40 @@
-from typing import Annotated, Optional
+from http import HTTPStatus
 
-from fastapi import APIRouter, Depends
-
-import logging
-
+from fastapi import HTTPException, APIRouter, Depends
 from pydantic import BaseModel
 
-from app.databases.mongo_db import MongoDBDatabase
-from app.databases.singletons import get_mongo_db
-from app.models.docs import DocsContent
+from app.api.routes.auth import get_current_user
+from app.auth.models.user import User
+import logging
+
+from app.chat_forms.models.appointment import Appointment
+from app.container import container
 
 logging.basicConfig(level=logging.DEBUG)
 
 router = APIRouter()
 
-db_dep = Annotated[MongoDBDatabase, Depends(get_mongo_db)]
+class PageDto(BaseModel):
+    collection_name: str
 
-class ContentDto(BaseModel):
-    base_url:str
-    link: str
+@router.post("/get_appointments_page/{page}", status_code=HTTPStatus.CREATED)
+async def get_appointments_page(
+        page_dto: PageDto,
+        page: int = 1,
+        # current_user: User = Depends(get_current_user)
+):
+    mdb = container.mdb()
 
-@router.get("/get_all_content_data")
-async def get_all_content_data(base_url: str,mdb: db_dep):
-    entries = await mdb.get_entries(
-        class_type=DocsContent,
-        doc_filter={"base_url": base_url,},
-    )
-    return entries
+    try:
+        appointments, total = await mdb.get_paginated_entries(
+            class_type=None,
+            collection_name=page_dto.collection_name,
+            page=page,
+            page_size=5,
+            # doc_filter={"email": current_user.email}
+        )
+    except Exception as e:
+        logging.error(f"Failed to get paginated appointments: {e}")
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Failed to get paginated appointments")
 
-@router.get("/get_content_data_by_link")
-async def get_content_data_by_link(base_url:str, link: str,mdb: db_dep):
-    entries = await mdb.get_entries(
-        class_type=DocsContent,
-        doc_filter={"base_url": base_url, "link": link},
-    )
-    print(entries)
-    return entries
+    return appointments, total
