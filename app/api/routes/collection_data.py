7e1ac4e609779f53f70184a43aeeea4f72147995
Nikola Nikolovski <nikolovski.nikola42@gmail.com>
2025-03-11 22:26:29 +0100

    Changed the name of the endpoint get_appointment_data to a more generic name: get_collection_data

--- app/api/routes/collection_data.py
+++ app/api/routes/collection_data.py
@@ -17,8 +17,8 @@ router = APIRouter()
 class PageDto(BaseModel):
     collection_name: str
 
-@router.post("/get_appointments_page/{page}", status_code=HTTPStatus.CREATED)
-async def get_appointments_page(
+@router.post("/get_collection_data_page/{page}", status_code=HTTPStatus.CREATED)
+async def get_collection_data_page(
         page_dto: PageDto,
         page: int = 1,
         # current_user: User = Depends(get_current_user)
@@ -33,6 +33,7 @@ async def get_appointments_page(
             page_size=5,
             # doc_filter={"email": current_user.email}
         )
+        print(len(appointments))
     except Exception as e:
         logging.error(f"Failed to get paginated appointments: {e}")
         raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="Failed to get paginated appointments")
